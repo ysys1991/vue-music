bf9aaa15559364d55e1096c99addeec7015cd836
ys19910330 <380576508@qq.com>
2019-03-07 15:58:40 +0800

    first commit

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+    <h1 class="text">Chicken Music</h1>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+    .m-header
+      position: relative
+      height: 44px
+      text-align: center
+      color: $color-theme
+      font-size: 0
+      .icon
+        vertical-align: top
+        display: inline-block
+        margin-top 6px
+        margin-right: 9px
+        width: 30px
+        height 32px
+        bg-image('logo')
+        background-size: 30px 32px
+      .text
+        vertical-align: top
+        display inline-block
+        line-height: 44px
+        font-size: $font-size-large
+</style>
